9ea4c0d558ee4187c978ee739a294b51b8eeae22
arttnba3 <arttnba3@outlook.com>
2021-10-13 10:12:56 -0700

    add output function

---
+++ kernel/lib/kernel/print.h
@@ -0,0 +1,6 @@
+#ifndef __PRINT_KERNEL_H
+#define __PRINT_KERNEL_H
+#include "stdint.h"
+void put_char(uint8_t ascii_chr);
+void put_str(char * msg);
+#endif
--- kernel/main.c
+++ kernel/main.c
@@ -1,21 +1,12 @@
+#include "print.h"
+
+char str[] = "arttnba3";
 int main (void)
 {   
-    while (1)
+    put_str("Hello \n World!\n");
+    while(1)
     {
-        asm(
-        "mov byte ptr [gs:170], 'W';"
-        "mov byte ptr [gs:172], 'E';"
-        "mov byte ptr [gs:174], 'L';"
-        "mov byte ptr [gs:176], 'C';"
-        "mov byte ptr [gs:178], 'O';"
-        "mov byte ptr [gs:180], 'M';"
-        "mov byte ptr [gs:182], 'E';"
-        "mov byte ptr [gs:186], 'T';"
-        "mov byte ptr [gs:188], 'O';"
-        "mov byte ptr [gs:192], 'A';"
-        "mov byte ptr [gs:194], '3';"
-        "mov byte ptr [gs:196], 'O';"
-        "mov byte ptr [gs:198], 'S';"
-        "mov byte ptr [gs:200], '!';");
+
     }
+    return 0;
 }
